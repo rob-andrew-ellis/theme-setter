a6847bddb5114debadb0ac9c8b15c4c3a092e34f
rob_andrew_ellis <robandrewellis@gmail.com>
2024-07-28 17:08:23 +0100

    Initial Commit

---
+++ set-light-mode.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Set GTK theme
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+
+# Generate colour scheme
+wal -i $1 -n -l
+
+# Set wallpaper
+nitrogen --set-zoom-fill $1 --head=0 && nitrogen --set-zoom-fill $1 --head=1
