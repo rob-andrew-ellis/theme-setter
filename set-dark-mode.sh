74f787e380d0868cd393f9b31e6cae1108f21dea
rob_andrew_ellis <robandrewellis@gmail.com>
2024-08-05 21:20:39 +0100

    changed set-dark-theme to set a custom wal theme rather than generating one from my wallpaper

--- set-dark-mode.sh
+++ set-dark-mode.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # Set GTK theme
-gsettings set org.gnome.desktop.interface gtk-theme Andromeda-dark
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
 # Generate colour scheme
-wal -i $1 -n
+# wal -i $1 -n
+wal --theme ~/.config/wal/colorschemes/dark/dkeg-stv-custom.json
 
 # Set wallpaper
 nitrogen --set-zoom-fill $1 --head=0 && nitrogen --set-zoom-fill $1 --head=1
--- set-light-mode.sh
+++ set-light-mode.sh
@@ -4,7 +4,7 @@
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
 
 # Generate colour scheme
-wal -i $1 -n -l
+wal -i $1 -n -l -b "FDF6E3"
 
 # Set wallpaper
 nitrogen --set-zoom-fill $1 --head=0 && nitrogen --set-zoom-fill $1 --head=1
